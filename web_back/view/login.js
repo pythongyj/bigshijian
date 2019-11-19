12d8c63635b7ed99deb3746cfeeda237e1b0cfe4
geyunjie <1329712473@qq.com>
2019-11-19 21:10:22 +0800

    增加模态框提示用户信息

--- web_back/view/login.js
+++ web_back/view/login.js
@@ -1,21 +1,23 @@
 var baseURL = "http://localhost:8000/";
-      $("#login").click(function() {
-        var text=$('[type=text]').val();
-        var password=$('[type=password]').val();
-        console.log(text);
-        
-        $.post(`${baseURL}admin/login`, 
-            {
-                user_name:text,
-                password:password 
-            },
-            function (res) {
-               if (res.code===200) {
-                   location.href='./index.html';
-               }else{
-                   alert(res.msg)
-               }
-                
+$("#login").click(function () {
+    var text = $('[type=text]').val();
+    var password = $('[type=password]').val();
+    console.log(text);
+
+    $.post(`${baseURL}admin/login`, {
+            user_name: text,
+            password: password
+        },
+        function (res) {
+            if (res.code === 200) {
+                location.href = './index.html';
+            } else {
+                $('#msgInfo').text(res.msg)
+                $('#myModal').modal('show')
+                // alert(res.msg)
+
             }
-        );
-      });
+
+        }
+    );
+});
