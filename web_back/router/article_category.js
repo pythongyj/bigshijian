fe212955188d5b419817bb7e1d96f3c5d254c047
geyunjie <1329712473@qq.com>
2019-11-22 08:56:07 +0800

    文章分类管理模块   编辑和删除  功能模块

--- web_back/router/article_category.js
+++ web_back/router/article_category.js
@@ -6,8 +6,20 @@ var articleCategory = {
             callback(res)
         })
     },
+    // 添加文章分类列表
     add:function (name,slug,callback) {
         $.post(APIURLS.category_add,{name:name,slug:slug},function (res) {
+            callback(res)
+        })
+    },
+    // 修改  编辑文章分类列表
+    edit:function (id,name,slug,callback) {
+        $.post(APIURLS.category_edit,{id:id,name:name,slug:slug},function (res) {
+            callback(res)
+        })
+    },
+    delete:function (id,callback) {
+        $.post(APIURLS.category_delete,{id:id},function (res) {
             callback(res)
         })
     }
--- web_back/view/article_category.js
+++ web_back/view/article_category.js
@@ -19,6 +19,7 @@ $('#model_add').click(function () {
         if (res.code === 200) {
             alert(res.msg)
             $("#addModal").modal("hide");
+            getShow()
         } else {
             alert(res.msg)
         }
@@ -27,4 +28,51 @@ $('#model_add').click(function () {
 //  点击取消  取消弹出框
 $('#model_shutoff').click(function () {
     $("#addModal").modal("hide");
+})
+
+//点击编辑按钮  进行编辑
+$('#showList').on('click', '.edit', function () {
+    var id = $(this).attr('data-id').trim()
+    var name = $(this).attr('data-name').trim()
+    var slug = $(this).attr('data-slug').trim()
+
+    $('.editId').val(id)
+    $('.editName').val(name)
+    $('.editSlug').val(slug)
+    $("#editModal").modal("show");
+})
+
+// 编辑   取消编辑
+$('#model_shutoff_edit').click(function () {
+    $("#editModal").modal("hide");
+})
+
+// 编辑   保存编辑内容
+$('#model_edit').click(function () {
+    var Pid = $('.editId').val().trim()
+    var Pname = $('.editName').val().trim()
+    var Pslug = $('.editSlug').val().trim()
+    articleCategory.edit(Pid, Pname, Pslug, function (res) {
+        if (res.code === 200) {
+            alert('保存成功')
+            $("#editModal").modal("hide");
+        } else {
+            alert(res.msg)
+        }
+    })
+})
+
+
+// 点击删除按钮   进行删除
+$('#showList').on('click', '.delete', function () {
+    if (confirm('你确定要删除？') === '') {
+        // 取消删除
+        return;
+    }
+    // 发起请求  删除文章分类中的单个分类内容
+    articleCategory.delete($(this).attr('data-id'), function (res) {
+        console.log(res);
+        // 刷新页面
+        getShow()
+    })
 })
